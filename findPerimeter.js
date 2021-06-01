0e96bf420c8a88a8694af7a962b09e4819237067
Geiger <geiga@shopbop.com>
2021-05-31 20:14:16 -0500

    adding exercises

---
+++ findPerimeter.js
@@ -0,0 +1,9 @@
+function findPerimeter(l, w) {
+    let p = l * 2 + w * 2;
+    console.log(p);
+    return p;
+}
+
+findPerimeter(6, 7);
+findPerimeter(20, 10);
+findPerimeter(2, 9);
---
+++ giveMeSomething.js
@@ -0,0 +1,7 @@
+function giveMeSomething(str) {
+    console.log("something " + str);
+}
+
+giveMeSomething("is better than nothing");
+giveMeSomething("Bob Jane");
+giveMeSomething("something something");
