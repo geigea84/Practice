14926c47be60e750be0b9e229fc7246c92735e99
Geiger <geiga@shopbop.com>
2021-05-31 19:54:58 -0500

    adding setColor

---
+++ setColor.js
@@ -0,0 +1,11 @@
+for (i = 0; i < 100; i++) {
+    if (i < 100/3) {
+        console.log("red");
+    }
+    else if (i < 200/3) {
+        console.log("green");
+    }
+    else {
+        console.log("blue");
+    }
+}
