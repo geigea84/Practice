b727a116bbac9defdcf24b0ab74d1510d06e7f5a
Geiger <geiga@shopbop.com>
2021-06-03 15:58:39 -0500

    solved factorials.js

--- factorials.js
+++ factorials.js
@@ -5,16 +5,20 @@ let numArr = [];
 function factorials(n) {
     if (n > 0) {
         numArr.push(n);
-        console.log(numArr);
+        //console.log(numArr);
         n = n - 1;
         factorials(n);
     }
     else {
+        //console.log(numArr);
         let product = numArr.reduce(function (a, b) {
             return a * b;
-        }, 0);
+        }, 1);
         console.log(product);
     }
 }
 
-factorials(5);
+//factorials(5);
+//factorials(3);
+//factorials(1);
+factorials(0);
