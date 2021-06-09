03dc6b933b83634943499f6576a522955858290e
Geiger <geiga@shopbop.com>
2021-06-08 20:52:03 -0500

    fixed minMax, error was with let format for console log

--- minMax.js
+++ minMax.js
@@ -8,19 +8,19 @@ let max;
 function minMax(numArr) {
     min = numArr[0];
     max = numArr[0];
-    let minMaxArr = [min, max];
 
     for (i = 0; i < numArr.length; i++) {
         if (min > numArr[i]) {
             min = numArr[i];
         }
-        else if (numArr[i] > max) {
+        if (numArr[i] > max) {
             max = numArr[i];
         }
     }
-    console.log(minMaxArr);
+    console.log([min, max]);
 }
 
-minMax([1, 2, 3, 4, 5]);
+//minMax([1, 2, 3, 4, 5]);
 //minMax([2334454, 5]);
 //minMax([1]);
+minMax([1, 0, 3, 2]);
