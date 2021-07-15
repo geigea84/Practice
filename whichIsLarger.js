38bd231a1cd68dbd53ba58bd2576e8aa985e2d71
Geiger <geiga@shopbop.com>
2021-07-15 12:56:41 -0500

    cleaned up testing format and feedback

--- whichIsLarger.js
+++ whichIsLarger.js
@@ -16,13 +16,16 @@
 
 function whichIsLarger(f, g) {
     if (f > g) {
-        console.log("f");
+        // console.log("f");
+        return "f";
     }
     else if (g > f) {
-        console.log("g");
+        // console.log("g");
+        return "g";
     }
     else {
-        console.log("neither");
+        // console.log("neither");
+        return "neither";
     }
 }
 
@@ -34,35 +37,119 @@ function g() {
     return 5;
 }
 
-//whichIsLarger(() => 10, () => 5);
-//expected f, returns g FAIL
+const funArr = [
+    whichIsLarger(() => 10, () => 5),
+    whichIsLarger(() => 100, () => 10),
+    whichIsLarger(() => 100, () => 50),
+    whichIsLarger(() => 100, () => 5),
+    whichIsLarger(() => 1000, () => 2),
+    whichIsLarger(() => 2, () => 1000),
+    whichIsLarger(() => 2, () => 1),
+    whichIsLarger(() => 1, () => 2),
+    whichIsLarger(5, 10),
+    whichIsLarger(5, 5),
+    whichIsLarger(f(), g())
+]
 
-//whichIsLarger(() => 100, () => 10);
-//expected f, returns f
+const expectedArr = [
+    "f", "f", "f", "f", "f", "g", "f", "g", "g", "neither", "f"
+]
 
-//whichIsLarger(() => 100, () => 50);
-//expected f, returns g FAIL
+function testFunArr() {
+    for (let i = 0; i < funArr.length; i++) {
+        console.log(
+            "Test " + [i + 1] + "\n" + 
+            "Expected " + expectedArr[i] + ", " +
+            "returned " + funArr[i]
+        );
 
-//whichIsLarger(() => 100, () => 5);
-//expected f, returns g FAIL
+        if (expectedArr[i] == funArr[i]) {
+            console.log("SUCCESS" + "\n");
+        }
+        else {
+            console.log("FAIL" + "\n");
+        }
+    }
+}
 
-//whichIsLarger(() => 1000, () => 2);
-//expected f, returns g FAIL
+// function testAll() {
+//     console.log(
+//         "test 1" + "\n" +
+//         "expected f, returns "
+//     );
+//     whichIsLarger(() => 10, () => 5);
+//     //expected f, returns g FAIL
 
-//whichIsLarger(() => 2, () => 1000);
-//expected g, returns f FAIL
+//     console.log(
+//         "test 2" + "\n" +
+//         "expected f, returns "
+//     );
+//     whichIsLarger(() => 100, () => 10);
+//     //expected f, returns f
 
-//whichIsLarger(() => 2, () => 1);
-//expected f, returns f
+//     console.log(
+//         "test 3" + "\n" +
+//         "expected f, returns "
+//     );
+//     whichIsLarger(() => 100, () => 50);
+//     //expected f, returns g FAIL
 
-//whichIsLarger(() => 1, () => 2);
-//expected g, returns g
+//     console.log(
+//         "test 4" + "\n" +
+//         "expected f, returns "
+//     );
+//     whichIsLarger(() => 100, () => 5);
+//     //expected f, returns g FAIL
 
-//whichIsLarger(5, 10);
-//expected g, returns g
+//     console.log(
+//         "test 5" + "\n" +
+//         "expected f, returns "
+//     );
+//     whichIsLarger(() => 1000, () => 2);
+//     //expected f, returns g FAIL
 
-//whichIsLarger(5, 5);
-//expected neither, returns neither
+//     console.log(
+//         "test 6" + "\n" +
+//         "expected g, returns "
+//     );
+//     whichIsLarger(() => 2, () => 1000);
+//     //expected g, returns f FAIL
 
-//whichIsLarger(f(), g());
-//expected f, returns f
+//     console.log(
+//         "test 7" + "\n" +
+//         "expected f, returns "
+//     );
+//     whichIsLarger(() => 2, () => 1);
+//     //expected f, returns f
+
+//     console.log(
+//         "test 8" + "\n" +
+//         "expected g, returns "
+//     );
+//     whichIsLarger(() => 1, () => 2);
+//     //expected g, returns g
+
+//     console.log(
+//         "test 9" + "\n" +
+//         "expected g, returns "
+//     );
+//     whichIsLarger(5, 10);
+//     //expected g, returns g
+
+//     console.log(
+//         "test 10" + "\n" +
+//         "expected neither, returns "
+//     );
+//     whichIsLarger(5, 5);
+//     //expected neither, returns neither
+
+//     console.log(
+//         "test 11" + "\n" +
+//         "expected f, returns "
+//     );
+//     whichIsLarger(f(), g());
+//     //expected f, returns f
+// }
+
+//testAll();
+testFunArr();
