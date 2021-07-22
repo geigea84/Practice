22a8919f97805dfb82b46b66c192033abe6ded81
Geiger <geiga@shopbop.com>
2021-07-22 13:18:07 -0500

    canNest is working!

--- canNest.js
+++ canNest.js
@@ -10,12 +10,12 @@ let minOne;
 let minTwo;
 let maxOne;
 let maxTwo;
+let newArrOne;
+let newArrTwo;
 
-function canNest(arrOne, arrTwo) {
+function minMaxOne(arrOne) {
     minOne = arrOne[0];
-    minTwo = arrTwo[0];
     maxOne = arrOne[0];
-    maxTwo = arrTwo[0];
 
     for (let i = 0; i < arrOne.length; i++) {
         if (minOne > arrOne[i]) {
@@ -25,4 +25,44 @@ function canNest(arrOne, arrTwo) {
             maxOne = arrOne[i];
         }
     }
+
+    newArrOne = [minOne, maxOne];
+    return  newArrOne;
 }
+
+function minMaxTwo(arrTwo) {
+    minTwo = arrTwo[0];
+    maxTwo = arrTwo[0];
+
+    for (let i = 0; i < arrTwo.length; i++) {
+        if (minTwo > arrTwo[i]) {
+            minTwo = arrTwo[i];
+        }
+        if (arrTwo[i] > maxTwo) {
+            maxTwo = arrTwo[i];
+        }
+    }
+
+    newArrTwo = [minTwo, maxTwo];
+    return newArrTwo;
+}
+
+function canNest(arrOne, arrTwo) {
+    minMaxOne(arrOne);
+    minMaxTwo(arrTwo);
+
+    if (
+        newArrOne[0] > newArrTwo[0] &&
+        newArrOne[1] < newArrTwo[1]
+    ) {
+        console.log("true");
+        return true;
+    }
+    else {
+        console.log("false");
+        return false;
+    }
+}
+
+//canNest([1, 2, 3, 4], [0, 6]);
+canNest([1, 2, 3, 4], [2, 3]);
