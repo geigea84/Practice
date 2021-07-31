004c4f0e3b9ddb907ff4b60ec778e8b957e595b0
Geiger <geiga@shopbop.com>
2021-07-30 20:02:10 -0500

    created sortDrinksByPrice, laid out formatting

---
+++ sortDrinksByPrice.js
@@ -0,0 +1,34 @@
+// https://edabit.com/challenge/nuXdWHAoHv9y38sn7
+
+// You will be given an array of drinks, with each 
+// drink being an object with two properties: name 
+// and price. Create a function that has the drinks 
+// array as an argument and return the drinks 
+// objects sorted by price in ascending order.
+
+const drinks = [
+    {
+        name: "red",
+        price: 30
+    },
+    {
+        name: "blue",
+        price: 50
+    },
+    {
+        name: "green",
+        price: 20
+    },
+    {
+        name: "purple",
+        price: 10
+    },
+    {
+        name: "orange",
+        price: 40
+    }
+];
+
+function sortDrinksByPrice(drinks) {
+
+}
