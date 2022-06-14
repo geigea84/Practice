b1d9b70473f9b7c8d9895de0890ce4e8a7fb173b
Aaron Geiger <geiga@amazon.com>
2022-06-14 09:09:28 -0500

    added and solved perimetersWithACatch

---
+++ perimetersWithACatch.js
@@ -0,0 +1,11 @@
+// https://edabit.com/challenge/WEvqZTFcHeYzFn74c
+
+function perimeter(l, num) {
+    console.log((l == "c" ? 6.28 : 4) * num);
+}
+
+perimeter("s", 7) // 28
+
+perimeter("c", 4) // 25.12
+
+perimeter("c", 9) // 56.52
